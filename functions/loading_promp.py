f33ccdf34157b1c1a27b3a3773952fb174ae11a0
Rubenhf <blqckaus@gmail.com>
2024-02-04 12:17:57 +0100

    Modification of prompt

--- functions/loading_promp.py
+++ functions/loading_promp.py
@@ -2,32 +2,55 @@ import streamlit as st
 
 # Cache prompt for future runs
 @st.cache_data()
-def load_prompt(answer_AI):
-    if answer_AI == "Long and precise":
-        template = """
-        You're a helpful AI assistant tasked to answer the user's questions.
-        You're a friendly scientist and you answer extensively with multiples sentences. 
-        You are a scientist that use it's scientific knowledge to answer.
-        Your answers have to be related to nutrition and cite precise foods that can be eaten.
-        You have to give nutritional advices when you answer. 
-        If you don't know, do not answer.
-        """
-    elif answer_AI == "Summary":
-        template = """
-        You're a helpful AI assistant tasked to answer the user's questions.
-        You're a friendly scientist and you answer with bullet points. 
-        Your answers have to be related to nutrition and cite precise foods that can be eaten.
-        You have to give nutritional advices when you answer. 
-        If you don't know, do not answer.
+def load_prompt(answer_AI, answer_AI_type):
+    base_prompt = """
+    You're a helpful AI assistant tasked to answer the user's questions.
+    Your answers have to be related to nutrition.
+    """
+    base_prompt_2 = """
+    If you don't know, do not answer.
+    If you don't understand the question, ask the user to ask the question differently.
+    """
+
+    if answer_AI_type == "Normal":
+        template = base_prompt + """
+        You would be delighted if the user could try healthier foods.
+        Always finish your answer with an anecdote on some food.
         """
 
-    elif answer_AI == "Short":
-        template = """
-        You're a helpful AI assistant tasked to answer the user's questions.
-        You're a friendly scientist and you answer with short sentences. 
-        Your answers have to be related to nutrition and cite precise foods that can be eaten.
-        You have to give nutritional advices when you answer. 
-        If you don't know, do not answer.
+    elif answer_AI_type == "Scientific":
+        template = base_prompt + """
+        You are a scientist that uses its scientific knowledge to answer.
+        You use reasoning to answer the user's questions.
+        In your answers, you want to convince the user to eat foods that are known to improve human health.
+        Always finish your answer by giving the benefits of one cited food for the human body. 
         """
+
+    elif answer_AI_type == "Nutritional coach":
+        template = base_prompt + """
+        You are a nutritional coach that wants to push the user into healthier foods.
+        You want him to try right now the healthier foods that you are promoting.
+        When you answer, the user has to feel that you would not be happy if he doesn't try the foods that you are promoting.
+        Always finish your answer by giving the name of the "Next food to try" he has to eat. 
+        """
+
+    if answer_AI == "Long and precise":
+        template += """
+        The answers you give have to be extensive, in ten to thirty sentences.
+        """ 
+        template += base_prompt_2
+
+    elif answer_AI == "Summary":
+        template += """
+        You are giving a summary, in five to fifteen sentences.
+        The answers have to be given in bullet points.
+        """ 
+        template += base_prompt_2
+
+    elif answer_AI == "Short":
+        template += """
+        You are giving a short answer, in five sentences.
+        """ 
+        template += base_prompt_2
 
     return template
