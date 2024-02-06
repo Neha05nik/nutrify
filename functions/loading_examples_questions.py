e0d0cfdf8eda05a041b9f41cfad4650cb5a3c071
Rubenhf <blqckaus@gmail.com>
2024-02-06 01:36:00 +0100

    Corrections

--- functions/loading_examples_questions.py
+++ functions/loading_examples_questions.py
@@ -1,11 +1,12 @@
 import random
+import os
 
 def random_questions(k_selected=4):
     """
     Function that randomly pick k_selected questions from the examples_questions.txt file
     Take k_selected, number of questions to return as argument
     """
-
+    print("Current Working Directory:", os.getcwd())
     # We read the examples_questions from the file and store them in a list
     with open("assets\examples_questions.txt", "r", encoding="utf-8") as file:
         questions = file.readlines()
